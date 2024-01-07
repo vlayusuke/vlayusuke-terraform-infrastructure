1349c823f43e974e4abfe8f8828ff8ae4912d45c
Yusuke TOMIOKA <yusuke.tomioka@arsaga.jp>
2024-01-07 17:24:10 +0900

    add: ベースとなるTerraformの基本設定を追加

---
+++ production/base_locals.tf
@@ -0,0 +1,28 @@
+# ===============================================================================
+# Base Local Value
+# ===============================================================================
+locals {
+  repository = "vlayusuke/vlayusuke-terraform-infrastructure"
+
+  env     = "prd"
+  project = "vlayusuke-terraform"
+  author  = "Yusuke TOMIOKA"
+
+  production_state_file  = "production.terraform.tfstate"
+  staging_state_file     = "staging.terraform.tfstate"
+  development_state_file = "development.terraform.tfstate"
+  root_state_file        = "terraform.tfstate"
+
+  region = "ap-northeast-1"
+
+  availability_zones = [
+    "ap-northeast-1a",
+    "ap-northeast-1c",
+  ]
+
+  domain = "vlayusuke.net"
+
+  database_name             = "vlayusuke"
+  database_master_user_name = "vlayusuke"
+}
+
